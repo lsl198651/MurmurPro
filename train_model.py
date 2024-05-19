7b012b46fb35f886580718962d180e9d0952d53b
shilong <1986510281@qq.com>
2024-05-19 15:30:54 +0800

    重命名代码文件

--- train_model.py
+++ train_model.py
@@ -6,14 +6,14 @@ import numpy as np
 from torch.utils.data.sampler import WeightedRandomSampler
 from torch.utils.data import DataLoader
 from model.resnet6v2.se_resnet import se_resnet6
-from model.resnet18.resnet18 import MyResnet18
+# from model.resnet18.resnet18 import MyResnet18
 from util.dataloaders_5fold import fold5_dataloader
-from util.traintest import train_test
+from traintest import train_test
 from util.BEATs_def import (logger_init, DatasetClass)
 # from util.dataloaders import get_features
 # from model.model_sknet import AudioClassifier
 # from BEATs import BEATs_Pre_Train_itere3
-from torchvision.models import resnet18
+# from torchvision.models import resnet18
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -51,8 +51,8 @@ if __name__ == '__main__':
     parser.add_argument("--beta", type=float, default=(0.9, 0.98), help="beta")
     parser.add_argument("--cross_evalue", type=bool, default=False)
     parser.add_argument("--train_fold", type=list,
-                        default=['0', '2', '3', '4'])
-    parser.add_argument("--test_fold", type=list, default=['1'])
+                        default=['0', '1', '3', '4'])
+    parser.add_argument("--test_fold", type=list, default=['2'])
     parser.add_argument("--setType", type=str, default=r"\13_baseset_4s_4k")
     parser.add_argument("--model_folder", type=str,
                         default=r"D:\Shilong\murmur\00_Code\LM\beats1\SE_ResNet6\MyModels")
