acc521114628bcfdb25670acec4e12aa62cd7c60
shilong <1986510281@qq.com>
2024-05-19 15:31:34 +0800

    新增提取特征文件和数据集操作文件，简化代码

---
+++ util/utils_dataset.py
@@ -0,0 +1,346 @@
+import os
+import shutil
+import random
+import librosa
+import matplotlib.pyplot as plt
+import librosa.display
+import soundfile
+import csv
+import numpy as np
+import pandas as pd
+from BEATs_def import mkdir
+import soundfile as sf
+from BEATs_def import get_wav_data
+import pandas as pd
+from helper_code import *
+from util.utils_features import get_features_mod, get_logmel_feature
+
+
+def copy_file(src_path, folder_path, patient_id_list, mur, position):
+    """将所有文件复制到目标目录"""
+    for patient_id in patient_id_list:
+        # for mur in murmur:
+        for pos in position:
+            target_dir = folder_path + "\\" + mur + "\\" + patient_id + "\\"
+            os.makedirs(target_dir, exist_ok=True)
+
+            txtname = src_path + "\\" + patient_id + ".txt"
+            wavname = src_path + "\\" + patient_id + pos + ".wav"
+            heaname = src_path + "\\" + patient_id + pos + ".hea"
+            tsvname = src_path + "\\" + patient_id + pos + ".tsv"
+
+            if os.path.exists(txtname):
+                shutil.copy(txtname, target_dir + "\\")
+            if os.path.exists(wavname):
+                shutil.copy(wavname, target_dir + "\\")
+            if os.path.exists(heaname):
+                shutil.copy(heaname, target_dir + "\\")
+            if os.path.exists(tsvname):
+                shutil.copy(tsvname, target_dir + "\\")
+
+
+def copy_wav_file(src_path, folder_path, patient_id_list, mur, position):
+    """将wav和tsv文件复制到目标目录"""
+    count = 0
+    # 1. make dir
+    mur_dir = folder_path + "\\" + mur
+    if not os.path.exists(mur_dir):
+        os.makedirs(mur_dir)
+    # 2. copy file
+    for patient_id in patient_id_list:
+        # for mur in murmur:
+        for pos in position:
+            target_dir = folder_path + "\\" + mur + "\\" + patient_id + "\\"
+            os.makedirs(target_dir, exist_ok=True)
+            wavname = src_path + "\\" + patient_id + pos + ".wav"
+            tsvname = src_path + "\\" + patient_id + pos + ".tsv"
+            txtname = src_path + "\\" + patient_id + ".txt"
+            if os.path.exists(wavname):
+                shutil.copy(wavname, target_dir + "\\")
+                count += 1
+            if os.path.exists(tsvname):
+                shutil.copy(tsvname, target_dir + "\\")
+            if os.path.exists(txtname):
+                shutil.copy(txtname, target_dir + "\\")
+    print("copy file num: ", count)
+
+
+# devide sounds into 4s segments
+def pos_dir_make(dir_path, patient_id, pos):
+    for po in pos:
+        subdir = dir_path + patient_id + "\\" + patient_id + po
+        wavname = subdir + ".wav"
+        if os.path.exists(wavname):
+            print("exist")
+            mkdir(subdir)  # make dir
+
+
+def index_load(tsvname):
+    """读取tsv文件内容,不需要close函数"""
+    with open(tsvname, "r") as f:
+        txt_data = f.read()
+    head = ["start", "end", "period"]
+    data = txt_data.split("\n")[:-1]
+    # 遍历每一行
+    for l in data:
+        sgmt = l.split("\t")
+        if sgmt[2] != "0":
+            head = np.vstack([head, sgmt])
+    return head[1:]
+
+
+# preprocessed PCGs were segmented into four heart sound states
+def period_div(
+    path,
+    mur,
+    patient_id_list,
+    positoin,
+    id_data,
+    Murmur_locations,
+    Systolic_murmur_timing,
+    Diastolic_murmur_timing,
+):
+
+    for patient_id in patient_id_list:
+        patient_dir_path = path + mur + patient_id + "\\" + patient_id
+        txtpath = patient_dir_path+".txt"
+        current_patient_data = load_patient_data(txtpath)
+        hunman_feat = get_features_mod(current_patient_data)
+        for pos in positoin:
+            dir_path = path + mur + patient_id + "\\" + patient_id + pos
+            tsv_path = dir_path + ".tsv"
+            wav_path = dir_path + ".wav"
+            index = id_data.index(patient_id)
+            wav_location = pos[1:]  # 听诊区域
+            locations = Murmur_locations[index].split("+")  # 有杂音的区域
+            # 此听诊区有杂音
+            if wav_location in locations:
+                murmur_type = "Present"
+                Systolic_state = Systolic_murmur_timing[index]
+                Diastolic_state = Diastolic_murmur_timing[index]
+                # 没有 Systolic murmur
+                Systolic_murmur = "Absent" if Systolic_state == "nan" else "Present"
+                # 没有 Diastolic murmur
+                Diastolic_murmur = "Absent" if Diastolic_state == "nan" else "Present"
+            # 此听诊区没有杂音
+            else:
+                murmur_type = "Absent"
+                Systolic_murmur = "Absent"
+                Diastolic_murmur = "Absent"
+                Systolic_state = "nan"
+                Diastolic_state = "nan"
+            # 如果是present的有杂音区域，或absent区域
+            # if (mur == "Absent\\") or (mur == "Present\\" and (wav_location in locations)):
+            if os.path.exists(tsv_path):
+                state_div2(
+                    tsv_path,
+                    wav_path,
+                    dir_path + "\\",
+                    patient_id + pos,
+                    murmur_type
+                    # Systolic_murmur,
+                    # Diastolic_murmur,
+                    # Systolic_state,
+                    # Diastolic_state,
+                    # hunman_feat
+                )
+
+
+def state_div(
+    tsvname,
+    wavname,
+    state_path,
+    index,
+    Systolic_murmur,
+    Diastolic_murmur,
+    Systolic_state,
+    Diastolic_state,
+    hunman_feat
+):
+    """切割出s1+收缩和s2+舒张"""
+    index_file = index_load(tsvname)
+    recording, fs = librosa.load(wavname, sr=4000)
+    num = 0
+    # start_index1 = 0
+    # end_index1 = 0
+    # start_index2 = 0
+    # end_index2 = 0
+    # count = 0
+    for i in range(index_file.shape[0] - 3):
+        # if count == 20:
+        #     break
+        if index_file[i][2] == "1" and index_file[i + 2][2] == "3":
+            start_index1 = float(index_file[i][0]) * fs
+            end_index1 = float(index_file[i+1][1]) * fs
+            start_index2 = float(index_file[i + 2][0]) * fs
+            end_index2 = float(index_file[i + 3][1]) * fs
+            num = num + 1
+
+            #  解决出现_0.wav的问题
+            print(start_index1, end_index1, start_index2, end_index2)
+            print("=============================================")
+            print("wav name: " + wavname)
+            buff1 = recording[int(start_index1): int(end_index1)]  # 字符串索引切割
+            buff2 = recording[int(start_index2): int(end_index2)]  # 字符串索引切割
+            print("buff1 len: " + str(len(buff1)),
+                  "buff2 len: " + str(len(buff2)))
+            # if Systolic_murmur == "Present" and Diastolic_murmur == "Absent":
+            #     # 切收缩期
+            #     soundfile.write(
+            #         state_path
+            #         + f"{index}_s1+Systolic_{num}_{Systolic_murmur}_{Systolic_state}_{hunman_feat}.wav",
+            #         buff1,
+            #         fs,
+            #     )
+            # else:
+            # 切收缩期
+            soundfile.write(
+                state_path
+                + f"{index}_s1+Systolic_{num}_{Systolic_murmur}_{Systolic_state}_{hunman_feat}.wav",
+                buff1,
+                fs,
+            )
+            # 切舒张期
+            soundfile.write(
+                state_path
+                + f"{index}_s2+Diastolic_{num}_{Diastolic_murmur}_{Diastolic_state}_{hunman_feat}.wav",
+                buff2,
+                fs,
+            )
+            # count += 1
+
+
+def state_div2(
+    tsvname,
+    wavname,
+    state_path,
+    id_pos,
+    murmur_type
+
+):
+    """按照4s切片"""
+    index_file = index_load(tsvname)
+    spilt_len = 4  # 切割长度为spilt_len s
+    recording, fs = librosa.load(wavname, sr=4000)
+    start = float(index_file[0][0]) * fs
+    start = float(index_file[-1][1]) * fs
+    buff = recording[int(start): int(start)]  # 要切割的数据
+    # 将buff数据切割为3s的数据
+    # 计算音频的总长度（秒）
+    # total_length = len(recording) / fs
+
+    # 计算每个片段的样本数
+    samples_per_segment = spilt_len * fs
+
+    # 切割音频数据
+    segments = []
+    for start in np.arange(0, len(recording), samples_per_segment):
+        end = start + samples_per_segment
+        segment = recording[start:end]
+        segments.append(segment)
+
+    segments.pop()
+    for i, segment in enumerate(segments):
+        soundfile.write(
+            state_path
+            + "{}_{}_{}_{}_{}.wav".format(
+                id_pos, str(spilt_len)+"s", i, murmur_type, "none"
+            ),
+            segment,
+            fs,
+        )
+
+
+# get patient id from csv file
+def get_patientid(csv_path):
+    # 'import csv' is required
+    with open(csv_path) as csvfile:
+        reader = csv.DictReader(csvfile)
+        id = [row["0"] for row in reader]  # weight 同列的数据
+        return id
+
+
+def fold_devide(data, flod_num=5):
+    """五折交叉验证
+    将输入列表打乱，然后分成五份
+    output: flod5 = {0:[],1:[],2:[],3:[],4:[]}
+    """
+    # 打乱序列
+    random.shuffle(data)
+    # 五折
+    flod5 = {}
+    point = []
+    for i in range(flod_num):
+        point.append(i*round(len(data)/flod_num))
+    # print(point)
+    # 分割序列
+    for i in range(len(point)):
+        if i < len(point)-1:
+            flod5[i] = []
+            flod5[i].extend(data[point[i]:point[i+1]])
+        else:
+            flod5[i] = []
+            flod5[i].extend(data[point[-1]:])
+    return flod5
+
+# copy data to folder
+
+
+def copy_states_data(patient_id, folder, type, murmur):
+    traget_path = folder+type+murmur
+    if not os.path.exists(traget_path):
+        os.makedirs(traget_path)
+    for id in patient_id:
+        dir_path = folder + murmur + id
+        print(dir_path)
+        for root, dir, file in os.walk(dir_path):
+            for subdir in dir:
+                subdir_path = os.path.join(root, subdir)
+                print(subdir_path)
+                if os.path.exists(dir_path):
+                    shutil.copytree(subdir_path, traget_path + subdir)
+                else:
+                    print("dir not exist")
+
+
+def data_set(root_path):
+    """数据增强，包括时间拉伸和反转"""
+    # root_path = r"D:\Shilong\murmur\01_dataset\06_new5fold"
+    npy_path_padded = root_path+r"\npyFile_padded\npy_files01_norm"
+    index_path = root_path + r"\npyFile_padded\index_files01_norm"
+    if not os.path.exists(npy_path_padded):
+        os.makedirs(npy_path_padded)
+    if not os.path.exists(index_path):
+        os.makedirs(index_path)
+    for k in range(5):
+        mel_list = []
+        src_fold_root_path = root_path+r"\fold_set_"+str(k)
+        # TODO 是否做数据增强
+        # data_Auge(src_fold_root_path)
+        for folder in os.listdir(src_fold_root_path):
+            dataset_path = os.path.join(src_fold_root_path, folder)
+            if k == 0 and folder == "absent":
+                wav, label, names, index, data_id, feat = get_wav_data(
+                    dataset_path, num=0)  # absent
+            else:
+                wav, label, names, index, data_id, feat = get_wav_data(
+                    dataset_path, data_id)  # absent
+            for i in range(len(wav)):
+                mel = get_logmel_feature(wav[i])
+                mel_list.append(mel)
+            np.save(npy_path_padded +
+                    f"\\{folder}_mel_norm01_fold{k}.npy", mel_list)
+            np.save(npy_path_padded +
+                    f"\\{folder}_wav_norm01_fold{k}.npy", wav)
+            np.save(npy_path_padded +
+                    f"\\{folder}_labels_norm01_fold{k}.npy", label)
+            np.save(npy_path_padded +
+                    f"\\{folder}_index_norm01_fold{k}.npy", index)
+            np.save(npy_path_padded +
+                    f"\\{folder}_name_norm01_fold{k}.npy", names)
+            np.save(npy_path_padded +
+                    f"\\{folder}_feat_norm01_fold{k}.npy", feat)
+            absent_train_dic = zip(index, names, feat)
+            pd.DataFrame(absent_train_dic).to_csv(
+                index_path+f"\\fold{k}_{folder}_disc.csv", index=False, header=False)
+    print("data set is done!")
---
+++ util/utils_features.py
@@ -0,0 +1,205 @@
+import shutil
+import json
+import os
+import torch
+import torch.nn as nn
+import os
+import shutil
+import librosa
+import librosa.display
+import numpy as np
+from helper_code import *
+
+
+def get_mfcc(wavform):
+    """提取wav的mfcc特征
+
+    Args:
+        wavform (float): 心音的时域信号
+    """
+    # 读取音频文件
+    # y, sr = librosa.load(wav_file, sr=None)
+    # 提取mfcc特征
+    mfccs = librosa.feature.mfcc(wavform, sr=4000, n_mfcc=40)
+    # return mfccs
+    return mfccs
+
+
+def get_logmel_feature(wavform, fs=4000):
+    """提取logmel特征"""
+    # 读取音频文件
+    # y, sr = librosa.load(wavform, sr=4000)
+    # 提取特征
+    mel = librosa.feature.melspectrogram(y=wavform, sr=fs, n_mels=128)
+    logmel = librosa.power_to_db(mel, ref=np.max)
+    return logmel
+
+
+def get_lami_angle(wavform):
+    """求格拉姆角场特征
+
+    Args:
+        wavform (_type_): 心音始于信号
+    """
+
+
+def get_sample_entropy(wavform):
+    """求样本熵特征
+
+    Args:
+        wavform (float32): 心音时域信号
+    """
+    # 读取音频文件
+    # y, sr = librosa.load(wav_file, sr=None)
+    # 提取样本熵特征
+    sample_spre = entropy.sample_entropy(wavform)
+    # return sample_spre
+    return sample_spre
+
+
+def get_distribution_entropy(wavform):
+    """求分布熵特征
+
+    Args:
+        wavform (float): 时域信号
+    """
+    # 读取音频文件
+    # y, sr = librosa.load(wav_file, sr=None)
+    # 提取分布熵特征
+    distrub_spre = entropy.distribution_entropy(wavform)
+    # return distrub_spre
+    return distrub_spre
+
+
+def get_fuzzy_entropy(wavform):
+    """求模糊熵特征
+
+    Args:
+        wavform (float): _description_
+    """
+    # 读取音频文件
+    # y, sr = librosa.load(wav_file, sr=None)
+    # 提取模糊熵特征
+    mohu_spre = entropy.fuzzy_entropy(wavform)
+    # return mohu_spre
+    return mohu_spre
+
+
+def get_gelamr_abgle(wavform):
+    """求格拉莫角域特征
+
+    Args:
+        wavform (_type_): _description_
+    """
+    # 读取音频文件
+    # y, sr = librosa.load(wav_file, sr=None)
+    # 提取格拉莫角域特征
+    gelamr_abgle = entropy.gelamr_abgle(wavform)
+    # return gelamr_abgle
+    return gelamr_abgle
+
+
+def get_time_feature(wavform):
+    """求信号的时域特征
+
+    Args:
+        wavform (_type_): _description_
+    """
+
+
+def get_frequency_feature(wavform):
+    """求信号的频域特征
+
+    Args:
+        wavform (_type_): _description_
+    """
+
+
+def get_features_mod(data):
+    # Extract the age group, sex and the pregnancy status features
+    age_group = get_age(data)
+    age_list = ['Neonate', 'Infant', 'Child', 'Adolescent', 'Young Adult']
+    is_pregnant = get_pregnancy_status(data)
+    if age_group not in ['Neonate', 'Infant', 'Child', 'Adolescent', 'Young Adult']:
+        if is_pregnant:
+            age = 'Young Adult'
+        else:
+            age = 'Child'
+    else:
+        age = age_group
+    age_fea = str(age_list.index(age))
+    # Extract sex. Use one-hot encoding.
+    sex = get_sex(data)
+    if compare_strings(sex, 'Female'):
+        sex_features = '0'
+    elif compare_strings(sex, 'Male'):
+        sex_features = '1'
+    if is_pregnant:
+        preg_fea = '1'
+    else:
+        preg_fea = '0'
+    return age_fea + sex_features + preg_fea
+
+
+class Params():
+    def __init__(self, json_path):
+        with open(json_path) as f:
+            params = json.load(f)
+            self.__dict__.update(params)
+
+    def save(self, json_path):
+        with open(json_path, 'w') as f:
+            params = json.dump(self.__dict__, f, indent=4)
+
+    def update(self, json_path):
+        with open(json_path) as f:
+            params = json.load(f)
+            self.__dict__.update(params)
+
+    @property
+    def dict(self):
+        return self.__dict__
+
+
+class RunningAverage():
+    def __init__(self):
+        self.total = 0
+        self.steps = 0
+
+    def update(self, loss):
+        self.total += loss
+        self.steps += 1
+
+    def __call__(self):
+        return (self.total/float(self.steps))
+
+
+def save_checkpoint(state, modelname, split, checkpoint):
+    filename = os.path.join(checkpoint, 'last{}.pth.tar'.format(split))
+    if not os.path.exists(checkpoint):
+        print("Checkpoint Directory does not exist")
+        os.mkdir(checkpoint)
+    torch.save(state, filename)
+    shutil.copyfile(filename, os.path.join(
+        checkpoint, f"{modelname}_model_best_{split}.pth.tar"))
+
+
+def load_checkpoint(checkpoint, model, optimizer=None, parallel=False):
+    if not os.path.exists(checkpoint):
+        raise ("File Not Found Error {}".format(checkpoint))
+    checkpoint = torch.load(checkpoint)
+    if parallel:
+        model.module.load_state_dict(checkpoint["model"])
+    else:
+        model.load_state_dict(checkpoint["model"])
+
+    if optimizer:
+        optimizer.load_state_dict(checkpoint["optimizer"])
+    return model
+
+
+def initialize_weights(m):
+    classname = m.__class__.__name__
+    print(classname)
+    if classname.find('Linear') != -1:
+        nn.init.ones_(m.weight.data)
